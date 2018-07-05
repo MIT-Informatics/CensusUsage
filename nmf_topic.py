c2783b044272f3ab9d6079ecaa471af71612647a
Dylan Sam <dylan_sam@brown.edu>
2018-07-05 11:49:18 -0700

    changed params

--- nmf_topic.py
+++ nmf_topic.py
@@ -8,11 +8,13 @@ from process import *
 # dataset = fetch_20newsgroups(shuffle=True, random_state=1,
 #                              remove=('headers', 'footers', 'quotes'))
 
-
 #setting paramteres for training
-features = 10
-components = 10
-number_topics = 5
+#number of top features that appear in corpus to be considered
+number_features = 1000
+#number of topics to print eventually
+number_topics = 20
+#n-gram size to consider
+n_size = 2
 
 def print_topics(model, feature_names, n_top_words):
     
@@ -26,7 +28,7 @@ def print_topics(model, feature_names, n_top_words):
     for topic_idx, topic in enumerate(model.components_):
         message = "Topic #%d: " % topic_idx
         message += " ".join([feature_names[i]
-                             for i in topic.argsort()[:-number_topics - 1:-1]])
+                             for i in topic.argsort()[:-n_top_words - 1:-1]])
         print(message)
 
 def topic_analysis(text_list):
@@ -38,40 +40,42 @@ def topic_analysis(text_list):
 
     #Creating vectorizer to take raw text data and transform into tf-idf matrix
     tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2,
-                                       max_features=features,
+                                       max_features=number_features,
                                        stop_words='english')
 
     print("Fitting tfidf vectorizer to samples")
     tfidf = tfidf_vectorizer.fit_transform(text_list)
 
-
-    # Fit the NMF model
-    print("Fitting the NMF model (Frobenius norm) with tf-idf features, "
-          "n_samples=%d and n_features=%d..."
-          % (len(text_list), features))
-
-    nmf = NMF(n_components=components, random_state=1,
+    nmf = NMF(n_components=number_topics, random_state=1,
               alpha=.1, l1_ratio=.5).fit(tfidf)
 
     print("\nTopics in NMF model (Frobenius norm):")
     tfidf_feature_names = tfidf_vectorizer.get_feature_names()
     print((tfidf_feature_names))
-    print_topics(nmf, tfidf_feature_names, number_topics)
+    print_topics(nmf, tfidf_feature_names, n_size)
 
 if __name__ == '__main__':
     
     #using twitter_act file to access Twitter API
     t = main()
     #searching for tweets with #ACS hashtag
-    tweets_objects = t.searchHashtag('uses census data')
+    # tweets_objects = t.searchHashtag('uses census data')
 
-    tweets_text = []
+    # tweets_text = []
 
     #adding only text into tweet_text list
-    for item in tweets_objects:
-        tweets_text.append(process_string(item['text']))
+    # for item in tweets_objects:
+    #     tweets_text.append(process_string(item['text']))
 
-    topic_analysis(tweets_text)
+    # topic_analysis(tweets_text)
     # data_samples = dataset.data[:n_samples]
+
+    words = [u'Hi what is your name.', u"Ever need to add some random or meaningless text into Microsoft Word to test a document, temporarily fill some space, or to see how some formatting looks? Luckily, Word provides a couple of quick and easy methods for entering random text into your document.", u"To do this, position the cursor at the beginning of a blank paragraph. Type the following and press Enter. It does not matter if you use lowercase, uppercase, or mixed case.", u"I'm a student and research assistant at Brown University, who is studying the fields of Computer Science and Applied Math. I am currently working in the Rubenstein Lab within the DARPA Molecular Informatics Project and as an intern in the MIT Program of Information Science. I am particularly interested in the ability to perceive information from data sets through analytical methods. I love research, data science, and machine learning. I am always looking for opportunities to learn new skills.", u"At school, I am on the organizing team of the Brown Data Science club and the Fintech at Brown, a member of the Brown Club Tennis Team, and a writer for the Ursa Sapiens Blog. In my free time, I love to play soccer and the viola."]
+
+    word_list = []
+    for item in words:
+        word_list.append(process_string(item))
+
+    topic_analysis(word_list)
 
     
